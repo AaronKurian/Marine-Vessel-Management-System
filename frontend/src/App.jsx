080e192355e07feee021e0f133e964fa95394744
diyajojo <diyaj2122@gmail.com>
2025-10-20 00:14:11 +0530

    refractoring codebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,9 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Auth from './pages/Auth'
 import Landing from './pages/Landing'
-import AddVessel from './components/utils/addvessel'
-import NewVoyage from './components/utils/addvoyage'
-import EditVoyage from './components/utils/editvoyage'
 import FleetDashboard from './pages/dashboard/fleetowner'
 import TraderDashboard from './pages/dashboard/trader'
 import CaptainDashboard from './pages/dashboard/captains'
-import AddVessel from './components/utils/addvessel'
 
 function App() {
 
@@ -21,10 +17,6 @@ function App() {
        <Route path="/dashboard/fleetowner" element={<FleetDashboard />} />
         <Route path="/dashboard/trader" element={<TraderDashboard />} />
        <Route path="/dashboard/captain" element={<CaptainDashboard />} />
-        <Route path="/addvessel" element={<AddVessel />} />
-        <Route path="/newvoyage" element={<NewVoyage />} />
-        <Route path="/editvoyage" element={<EditVoyage />} />
-        <Route path="/approvevoyage" element={<ApproveVoyage />} />
       </Routes>
     </Router>
   )
